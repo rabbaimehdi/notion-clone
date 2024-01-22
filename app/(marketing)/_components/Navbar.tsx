de9c807303c59ba8fcaaf23deb2b3b346ff27836
mehdi <mehdi.rabbai@gmail.com>
2024-01-22 21:10:13 +0100

    Add dark mode

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -3,6 +3,7 @@
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./Logo";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export const Navbar = () => {
   const scrolled = useScrollTop();
@@ -10,13 +11,14 @@ export const Navbar = () => {
   return (
     <div
       className={cn(
-        "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+        "z-50 bg-background dark: bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
         scrolled && "border-b shadow-sm"
       )}
     >
       <Logo></Logo>
+
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        Login
+        <ModeToggle></ModeToggle>
       </div>
     </div>
   );
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,6 +8,7 @@ export default function MarketingPage() {
     <div className="min-h-full flex flex-col">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading></Heading>
+
         <Heroes></Heroes>
       </div>
       <Footer></Footer>
